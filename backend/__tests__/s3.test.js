391787336d4c270139823df053dab52371da74c1
Markee Smith <markee.j.smith@gmail.com>
2020-07-22 12:51:01 -0400

    Added remaining backend tests for AWS. Will add more as functionality is added.

--- backend/__tests__/s3.test.js
+++ backend/__tests__/s3.test.js
@@ -1,5 +1,13 @@
 const s3 = require('../aws/s3');
 
-test('configureBucket sets "Bucket" param correctly', () => {
-    expect(s3.getBucket()).toBe(s3.configureBucket("MyBucket"));
+test('configureBucket sets AWS S3 "Bucket" param correctly', () => {
+    expect(s3.getBucket()).toBe(s3.configureBucket("thirdday"));
+});
+
+test('getAllObjects returns Contents', () => {
+    return expect(s3.getAllObjects()).resolves.toHaveProperty('Contents');
+});
+
+test('getSpecificObject returns Contents', () => {
+    return expect(s3.getSpecificObjects('gallery/homes/', '/')).resolves.toHaveProperty('Contents');
 });
