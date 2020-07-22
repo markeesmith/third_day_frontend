916b436af0410dcd1700c06d0d02213ae186dd07
Markee Smith <markee.j.smith@gmail.com>
2020-07-22 11:26:06 -0400

    Added base for AWS S3 support

---
+++ backend/aws/s3.js
@@ -0,0 +1,34 @@
+// Load the SDK and UUID
+const AWS  = require('aws-sdk');
+const uuid = require('uuid');
+var s3Obj = [];
+
+// S3 Parameters and Base Access
+var params = {};
+const baseAPI = new AWS.S3({apiVersion: '2006-03-01'});
+
+// S3 Functions
+s3Obj.setPrefix = (prefix) => {
+    params["Prefix"] = prefix;
+};
+
+s3Obj.setDelimiter = (delimiter) => {
+    params["Delimiter"] = delimiter;
+};
+
+s3Obj.configureBucket = (bucket) => {
+    params["Bucket"] = bucket;
+};
+
+s3Obj.getAllObjects = () => {
+    return objectPromise = baseAPI.listObjects(params).promise();
+};
+
+s3Obj.getSpecificObjects = (prefix, delimiter) => {
+    s3Obj.setPrefix(prefix);
+    s3Obj.setDelimiter(delimiter);
+
+    return objectPromise = baseAPI.listObjects(params).promise();
+};
+
+module.exports = s3Obj;
