76ce4108bfc868947aee094af4d17fc137df1279
Markee Smith <markee.j.smith@gmail.com>
2020-07-30 10:07:41 -0400

    Fixed Query.js to avoid return null error. Also implemented db forwarding for simple queries.

--- backend/src/resolvers/Query.js
+++ backend/src/resolvers/Query.js
@@ -1,19 +1,15 @@
+const { forwardTo } = require('prisma-binding');
+
 const Query = {
-    async customers(parent, args, ctx, info) {
-        const customers = await ctx.db.query.customers();
-        return customers;
-    },
+    customers: forwardTo('db'),
+    galleries: forwardTo('db'),
     async testimonials(parent, args, ctx, info) {
-        const testimonials = await ctx.db.query.testimonials();
+        const testimonials = await ctx.db.query.testimonials({}, info);
         return testimonials;
     },
     async ranks(parent, args, ctx, info) {
-        const ranks = await ctx.db.query.ranks();
+        const ranks = await ctx.db.query.ranks({}, info);
         return ranks;
-    },
-    async galleries(parent, args, ctx, info) {
-        const galleries = await ctx.db.query.galleries();
-        return galleries;
     }
 };
 
