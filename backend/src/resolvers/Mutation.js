08036ec20f164dcd711be8f4034c8d24f2dddb4e
Markee Smith <markee.j.smith@gmail.com>
2020-07-30 08:38:48 -0400

    Redid the database design, redeployed prisma, created some basic mutations and queries for testing graphQL setup

--- backend/src/resolvers/Mutation.js
+++ backend/src/resolvers/Mutation.js
@@ -7,7 +7,34 @@ const Mutation = {
         }, info);
         
         return customer;
-    }
+    },
+    async createTestimonial(parent, args, ctx, info) {
+        const testimonial = await ctx.db.mutation.createTestimonial({
+            data: {
+                ...args
+            }
+        }, info);
+
+        return testimonial;
+    },
+    async createRank(parent, args, ctx, info) {
+        const rank = await ctx.db.mutation.createRank({
+            data: {
+                ...args
+            }
+        }, info);
+
+        return rank;
+    },
+     async createGallery(parent, args, ctx, info) {
+         const gallery = await ctx.db.mutation.createGallery({
+             data: {
+                 ...args
+             }
+         }, info);
+
+         return gallery;
+     }
 };
 
 module.exports = Mutation;
--- backend/src/resolvers/Query.js
+++ backend/src/resolvers/Query.js
@@ -2,6 +2,18 @@ const Query = {
     async customers(parent, args, ctx, info) {
         const customers = await ctx.db.query.customers();
         return customers;
+    },
+    async testimonials(parent, args, ctx, info) {
+        const testimonials = await ctx.db.query.testimonials();
+        return testimonials;
+    },
+    async ranks(parent, args, ctx, info) {
+        const ranks = await ctx.db.query.ranks();
+        return ranks;
+    },
+    async galleries(parent, args, ctx, info) {
+        const galleries = await ctx.db.query.galleries();
+        return galleries;
     }
 };
 
