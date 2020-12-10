7650d47224c366e117e81c4cb2f3239da68fa7c8
Markee Smith <markee.j.smith@gmail.com>
2020-12-10 14:18:00 -0500

    Fixed query for individual gallery (ID_GALLERY_QUERY)

--- pages/gallery.js
+++ pages/gallery.js
@@ -3,17 +3,17 @@ import { withRouter } from 'next/router';
 import PropTypes from 'prop-types';
 import { Query } from 'react-apollo';
 import GalleryAllPhotos from '../components/GalleryAllPhotos';
-import { ID_GALLERIES_QUERY } from '../lib/gql';
+import { ID_GALLERY_QUERY } from '../lib/gql';
 
 const Gallery = ({ router }) => (
   <Query
-    query={ID_GALLERIES_QUERY}
+    query={ID_GALLERY_QUERY}
     variables={{ searchTerm: parseInt(router.query.id, 10) }}
   >
     {({ data, loading, error }) => {
       if (loading) return <p>Loading...</p>;
       if (error) return <p>Error: {error.message}</p>;
-      return <GalleryAllPhotos gallery={data.galleries[0]} />;
+      return <GalleryAllPhotos gallery={data.gallery} />;
     }}
   </Query>
 );
